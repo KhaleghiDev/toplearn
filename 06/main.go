89e702a6462d50c4481d02a967b15898147baecc
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2022-08-23 20:43:47 +0430

    staruct and map get value

--- 06/main.go
+++ 06/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+//struct
+type person struct{
+	name string
+	famile string
+	age  int
+}
+
 func main() {
 	//map ans stuct
 	var m = make(map[string]int)
@@ -12,12 +19,27 @@ func main() {
 	maps["tow"] = 2
 
 	fmt.Println(m, maps["tow "])
-	// _, ok := m["first"]// output =>true 
+	// _, ok := m["first"]// output =>true
 	v, ok := m["first"]
-	fmt.Println(ok,"value :", v)
+	fmt.Println(ok, "value :", v)
 
-	if v,ok:=m["secent"];ok {
-		fmt.Println("secent: " ,v)
+	if v, ok := m["secent"]; ok {
+		fmt.Println("secent: ", v)
 	}
+	//struct
 
+	reza := person{age:50,famile: "razavi",name:   "reza"}
+	fmt.Print(reza)
+
+	people:=map[string]person{
+		"ali":person{age:20,famile: "razavi",name: "ali"},
+		"mohammad":person{age:50,famile: "razavi",name:   "mohammad"},
+	}
+	fmt.Print("peple : ",people)
+	fmt.Print("ali : ",people["ali"])
+	fmt.Print("name : ",people["ali"].name)
 }
+	
+	
+
+
