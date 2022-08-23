70f399295536684340af987e84726c594faa287f
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2022-08-23 21:45:07 +0430

    add panic and  error handeler ==try cache

--- 07/main.go
+++ 07/main.go
@@ -13,6 +13,7 @@ func main() {
 	fmt.Println(*PI)
 	// incriment(&I)
 	fmt.Println(&I)
+	testPanic()
 	testDefer()
 }
 func incriment(p *int) {
@@ -22,4 +23,16 @@ func testDefer() {
 	defer fmt.Println("wold")
 	defer fmt.Println("wold2")
 	fmt.Println("Hello")
+}
+func testPanic(){
+
+	defer func ()  {//try cache handele error preventing crash 
+		if err:=recover();err!=nil{
+			fmt.Println("panic handeles :")
+			fmt.Println(err)
+		}
+	}()
+	fmt.Println("Hello")
+	panic("hahahahaa i am panic")
+	
 }
