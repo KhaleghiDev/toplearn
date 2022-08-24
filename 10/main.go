20afbb9f5ca3e3885c52062c6bec9b03a755869b
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2022-08-24 13:19:55 +0430

    use  interface,struct,pointer and method

---
+++ 10/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// method and interface
+type testInter interface {
+	sayHello()
+	say(s string)
+	increment()
+	GetValuePrint() int
+}
+type testruct struct {
+	i int
+}
+
+func (ts *testruct) sayHello() {
+	fmt.Print("Hello \n")
+}
+func (ts *testruct) say(input string) {
+	fmt.Println(input)
+}
+func (ts *testruct) increment() {
+	ts.i++
+}
+func (ts *testruct) GetValuePrint() int {
+	return ts.i
+}
+
+func main() {
+	var test testInter
+	test=&testruct{}
+	test.sayHello()
+	test.say("hi how are you !")
+	test.increment()
+	test.increment()
+	test.increment()
+	fmt.Println(test.GetValuePrint())
+
+}
