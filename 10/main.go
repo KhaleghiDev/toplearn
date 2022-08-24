cdd4f8a0e2d39e3ac4583631141e32bb4ae0356b
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2022-08-24 16:18:55 +0430

    metoed type value

--- 10/main.go
+++ 10/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // method and interface
 type testInter interface {
@@ -12,11 +14,12 @@ type testInter interface {
 type testruct struct {
 	i int
 }
-func newTStract() testInter{
+
+func newTStract() testInter {
 	return new(testruct)
 }
-func newTStractWithv(v int) testInter{
-	return &testruct{i:v}
+func newTStractWithv(v int) testInter {
+	return &testruct{i: v}
 }
 func (ts *testruct) sayHello() {
 	fmt.Print("Hello \n")
@@ -31,15 +34,54 @@ func (ts *testruct) GetValuePrint() int {
 	return ts.i
 }
 
+type emmbddingStract struct {
+	*testruct
+}
+
+func testEmpaty(v interface{}) {
+	// fmt.Println(v)
+	// if i,ok:=v.(int);ok {
+	// 	fmt.Println("I Am int my value is :",i)
+	// }else{
+	// 	fmt.Println("I Am not int :")
+	// }
+	switch val := v.(type) {
+	case int:
+		fmt.Println("int", val)
+	case float32:
+		fmt.Println("flout", val)
+	case string:
+		fmt.Println("string", val)
+	case bool:
+		fmt.Println("bool", val)
+	default:
+		fmt.Println("not int,float32,bool , string", val)
+	}
+}
+
 func main() {
 	var test testInter
-	test= newTStract()   //&testruct{}
-	test= newTStractWithv(5) 
+	test = newTStract() //&testruct{}
+	test = newTStractWithv(5)
 	test.sayHello()
 	test.say("hi how are you !")
 	test.increment()
 	test.increment()
 	test.increment()
 	fmt.Println(test.GetValuePrint())
+
+	te := emmbddingStract{testruct: &testruct{i: 800}}
+	te.say("hahahaaaaa...")
+	te.increment()
+	te.increment()
+	te.increment()
+	te.increment()
+	te.increment()
+	te.increment()
+	fmt.Println(te.GetValuePrint())
+	testEmpaty(5)
+	testEmpaty("string")
+	testEmpaty(te)
+	testEmpaty(&te)
 
 }
